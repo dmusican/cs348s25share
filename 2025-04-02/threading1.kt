d13ad7f54658c84a6dfa22b1b6582c3c638a6e5d
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-02 14:50:22 -0500

    Scripted auto-commit on change (2025-04-02 14:50:22) by gitwatch.sh

--- 2025-04-02/threading1.kt
+++ 2025-04-02/threading1.kt
@@ -1,14 +1,14 @@
 // built in class named Thread that you inherit from
-class ExampleThread : Thread() {
+class ExampleThread(val id: Int) : Thread() {
     override fun run() {
-        println("Hi from thread")
+        println("Hi from thread $id")
     }
 }
 
 fun main() {
     for (i in 0..<5) {
-        val myThread = ExampleThread()
+        val myThread = ExampleThread(i)
         myThread.start()
-        
+
     }
 }
