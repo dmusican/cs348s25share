5c5770875bf9a5bb01aecbf44c3ef774206f30c7
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-02 14:13:44 -0500

    Scripted auto-commit on change (2025-04-02 14:13:43) by gitwatch.sh

--- 2025-04-02/sample2.kt
+++ 2025-04-02/sample2.kt
@@ -1,7 +1,12 @@
 fun main() {
+    // by default, lists are immutable, can't be changed, so add doesn't work
     val things = listOf(12, 19, 27)
     println(things.count())
     for (item in things) {
         println(item)
     }
+
+    val morethings = mutableListOf(21, 29)
+    morethings.add(42)
+    println(morethings)
 }
