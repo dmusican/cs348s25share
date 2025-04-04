995693042245896e227d485ade78370ad9579a0a
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-04 14:45:40 -0500

    Scripted auto-commit on change (2025-04-04 14:45:40) by gitwatch.sh

--- 2025-04-04/Sample1.kt
+++ 2025-04-04/Sample1.kt
@@ -1,9 +1,17 @@
 // Easier approach. ?????? hmmmm
+// There's no clean way to return an answer
+// from the threads
+// So let's do something messy, bad, and fun, to show you what
+// you shouldn't do, but some of you won't listen and will pay
 import kotlin.concurrent.thread
 fun main() {
+    var counter = 0
     for (i in 0..<20) {
         val myThread = thread {
+            val updatedCounter = counter + 1
             println("Hello from thread $i")
+            counter = updatedCounter
+            println("Hello again from thread $i. Counter is now $counter.")
         }
     }
 }
