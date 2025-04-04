761f2ddaae11db4234de7268668c31ef8a048289
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-04 15:11:24 -0500

    Scripted auto-commit on change (2025-04-04 15:11:24) by gitwatch.sh

--- 2025-04-04/Sample2.kt
+++ 2025-04-04/Sample2.kt
@@ -12,17 +12,20 @@ class AnswerThread(var i: Int) : Thread() {
 fun main() {
     val allThreads = mutableListOf<AnswerThread>()
     for (i in 0..<20) {
+        // Only main thread has access to 'answer' in the object
+        // because thread hasn't started yet
         val myThread = AnswerThread(i)
         myThread.start()
+        // ok, uhoh, thread has started, main thread better
+        // not touch answer
         allThreads.add(myThread)
     }
-    // for (myThread in allThreads) {
-    //     myThread.join()  // wait for thread to finish
-    // }
 
     // Get answers from each when done
     for (myThread in allThreads) {
         myThread.join()
+        // main thread touches answer below, but only AFTER
+        // thread is guaranteed to be done
         println("Answer from ${myThread.i} is ${myThread.answer}")
     }
 
