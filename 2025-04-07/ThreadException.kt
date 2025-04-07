8470480bcfa665f7ca59fc55acbfeff67a9dfa48
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-07 14:20:49 -0500

    Scripted auto-commit on change (2025-04-07 14:20:49) by gitwatch.sh

---
+++ 2025-04-07/ThreadException.kt
@@ -0,0 +1,22 @@
+import kotlin.concurrent.thread
+fun doStuff() {
+    var counter = 0
+    for (i in 0..<200) {
+        val myThread = thread {
+            var counter = 0
+            for (i in 0..<1000) {
+                Thread.sleep(1)
+                counter++
+            }
+            println("Hello from thread $i. Counter is now $counter.")
+        }
+    }
+    val newThread = thread {
+        throw Exception("oops")
+    }
+}
+
+fun main() {
+    doStuff()
+    println("Successful")
+}
