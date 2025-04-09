d7354f0df4c14df939712c4bf31810aaf4c3c29f
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-09 14:23:29 -0500

    Scripted auto-commit on change (2025-04-09 14:23:29) by gitwatch.sh

--- 2025-04-07/forkjoin/src/main/kotlin/average.kt
+++ 2025-04-07/forkjoin/src/main/kotlin/average.kt
@@ -3,8 +3,9 @@ import kotlinx.coroutines.*
 // This program recursively finds the count of all of the prime values of an array
 // in parallel using Java's ForkJoin Framework.
 
-data class
-suspend fun countTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Int {
+data class Result(int count, int total)
+
+suspend fun countTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Result {
 
     if (hi - lo <= seqCutoff) {
         var ans = 0
