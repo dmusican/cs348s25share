0e9bd1d4bfaab212e69a80c7372a5a9670c671a1
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-09 14:21:06 -0500

    Scripted auto-commit on change (2025-04-09 14:21:06) by gitwatch.sh

--- 2025-04-07/forkjoin/src/main/kotlin/Example.kt
+++ 2025-04-07/forkjoin/src/main/kotlin/Example.kt
@@ -2,3 +2,8 @@ data class Student(val name: String,
                    val age: Int,
                    val id: Int,
                    val residence: String)
+
+fun main() {
+    val dave = Student("Dave", 15, 1234567, "here")
+    println(dave)
+}
