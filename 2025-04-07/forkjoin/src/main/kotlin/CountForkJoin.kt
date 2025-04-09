7cbcbca8ddd4eb2bd3f5374c1323fc0ed95899ad
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-09 13:25:04 -0500

    Scripted auto-commit on change (2025-04-09 13:25:04) by gitwatch.sh

--- 2025-04-07/forkjoin/src/main/kotlin/CountForkJoin.kt
+++ 2025-04-07/forkjoin/src/main/kotlin/CountForkJoin.kt
@@ -1,12 +1,9 @@
 import kotlinx.coroutines.*
 
-// This program recursively finds the sum of all of the prime values of an array
-// in parallel using Java's ForkJoin Framework. This example is originally fom
-// Dan Grossman's A Sophomoric Introduction to Shared-Memory Parallelism and
-// Concurrency, Chapter 3. (Dave Musicant modified it a lot, and converted it to
-// Kotlin.)
+// This program recursively finds the count of all of the prime values of an array
+// in parallel using Java's ForkJoin Framework.
 
-suspend fun sumTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Int {
+suspend fun countTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Int {
 
     if (hi - lo <= seqCutoff) {
         var ans = 0
@@ -17,8 +14,8 @@ suspend fun sumTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Int {
     } else {
         val mid = (lo + hi) / 2
         return coroutineScope {
-            val left = async { sumTask(arr, lo, mid, seqCutoff)}
-            val right = sumTask(arr, mid, hi, seqCutoff)
+            val left = async { countTask(arr, lo, mid, seqCutoff)}
+            val right = countTask(arr, mid, hi, seqCutoff)
             left.await() + right
         }
 
@@ -26,10 +23,10 @@ suspend fun sumTask(arr: List<Int>, lo: Int, hi: Int, seqCutoff: Int): Int {
 }
 
 // Sum the prime values in an array.
-fun sumMultiThreaded(arr: List<Int>, seqCutoff: Int): Int {
+fun countMultiThreaded(arr: List<Int>, seqCutoff: Int): Int {
     return runBlocking(Dispatchers.Default) {
         async {
-            sumTask(arr, 0, arr.count(), seqCutoff)
+            countTask(arr, 0, arr.count(), seqCutoff)
         }.await()
     }
 }
