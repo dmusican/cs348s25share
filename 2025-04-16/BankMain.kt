3fdef584380e0654d44632130f126581a0665f2e
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-16 14:43:50 -0500

    Scripted auto-commit on change (2025-04-16 14:43:50) by gitwatch.sh

---
+++ 2025-04-16/BankMain.kt
@@ -0,0 +1,24 @@
+import kotlin.concurrent.thread
+fun main() {
+    val acct = Bank4(0)
+    val numtimes = 100000
+    val t1 = thread {
+        for (i in 0..<numtimes) {
+            acct.deposit(1)
+            if (i % 10000 == 0) {
+                println("${acct.balance} Thread 1")
+            }
+        }
+    }
+    val t2 = thread {
+        for (i in 0..<numtimes) {
+            acct.withdraw(1)
+            if (i % 10000 == 0) {
+                println("${acct.balance} Thread 2")
+            }
+        }
+    }
+    t1.join()
+    t2.join()
+    println("At end ${acct.balance}")
+}
