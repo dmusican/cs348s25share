52e8e2dfeb9c748635308f296be1198346b179a0
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-16 14:43:44 -0500

    Scripted auto-commit on change (2025-04-16 14:43:44) by gitwatch.sh

---
+++ 2025-04-16/Bank1.kt
@@ -0,0 +1,18 @@
+class Bank1(_balance: Int) {
+
+    var balance = _balance
+        set(value) {
+            field = value
+        }
+        get() {
+            return field
+        }
+
+    fun withdraw(amt: Int) {
+        balance -= amt
+    }
+
+    fun deposit(amt: Int) {
+        balance += amt
+    }
+}
