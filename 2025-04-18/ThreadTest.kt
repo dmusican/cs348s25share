87aa98ee8b372df5b7c9976c82faae698253ad60
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 14:17:56 -0500

    Scripted auto-commit on change (2025-04-18 14:17:56) by gitwatch.sh

---
+++ 2025-04-18/ThreadTest.kt
@@ -0,0 +1,29 @@
+// https://dzone.com/articles/java-volatile-keyword0-
+
+
+import kotlin.concurrent.thread
+import kotlin.random.Random
+var sharedValue = 0
+
+fun main() {
+    thread {
+        var localValue = sharedValue
+        while (true){
+            if( localValue != sharedValue){
+                println("Read new sharedValue : $sharedValue")
+                localValue = sharedValue;
+            }
+
+        }
+    }
+
+    thread {
+        var localValue = sharedValue
+        while (true){
+            localValue++
+            println("Incrementing sharedValue to $localValue")
+            sharedValue = localValue
+            Thread.sleep(500)
+        }
+    }
+}
