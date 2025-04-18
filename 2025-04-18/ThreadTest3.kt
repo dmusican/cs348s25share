e3c5306714988375f3fbb30386a4180bf91bf152
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 14:48:54 -0500

    Scripted auto-commit on change (2025-04-18 14:48:54) by gitwatch.sh

--- 2025-04-18/ThreadTest3.kt
+++ 2025-04-18/ThreadTest3.kt
@@ -8,18 +8,12 @@ var sharedValue = 0
 fun main() {
     thread {
         var localValue = sharedValue
-        var count = 0L
         while (true){
             if( localValue != sharedValue){
                 println("Read new sharedValue : $sharedValue")
                 println("Count = $count")
                 localValue = sharedValue;
             }
-            count++
-            if (count % 10000000000L == 0L) {
-                System.exit(1)
-            }
-
         }
     }
 
