ec3d74fb848dd9b3866b17f7cd1630fa288fdb14
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-12 14:04:20 -0500

    Scripted auto-commit on change (2025-05-12 14:04:20) by gitwatch.sh

--- 2025-05-12/countdown-processes.py
+++ 2025-05-12/countdown-processes.py
@@ -5,14 +5,14 @@ def countdown(n):
     while n > 0:
         n -= 1
 
-COUNT = int(1e3)
+COUNT = int(1e8)
 
 t1 = mp.Process(target=countdown,args=(COUNT//2,))
 t2 = mp.Process(target=countdown,args=(COUNT//2,))
 start = time.time()
 t1.start()
-t2.start()
 t1.join()
+t2.start()
 t2.join()
 end = time.time()
 print(end-start)
