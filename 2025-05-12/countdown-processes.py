d3b4dfdf256576a979055e0754385dcb73aa7142
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-12 14:02:51 -0500

    Scripted auto-commit on change (2025-05-12 14:02:51) by gitwatch.sh

--- 2025-05-12/countdown-processes.py
+++ 2025-05-12/countdown-processes.py
@@ -4,10 +4,17 @@ import time
 def countdown(n):
     while n > 0:
         n -= 1
+        if n % 100 == 0:
+            time.sleep(1)
 
 COUNT = int(1e3)
 
+t1 = Thread(target=countdown,args=(COUNT//2,))
+t2 = Thread(target=countdown,args=(COUNT//2,))
 start = time.time()
-countdown(COUNT)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 end = time.time()
 print(end-start)
