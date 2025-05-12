1972b88ef989b0e676c31fb58858fad31f89b2e2
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-12 13:57:08 -0500

    Scripted auto-commit on change (2025-05-12 13:57:08) by gitwatch.sh

---
+++ 2025-05-12/countdown-sleep.py
@@ -0,0 +1,15 @@
+from threading import Thread
+import time
+
+def countdown(n):
+    while n > 0:
+        n -= 1
+        if n % 100 == 0:
+            time.sleep(1)
+
+COUNT = int(1e3)
+
+start = time.time()
+countdown(COUNT)
+end = time.time()
+print(end-start)
