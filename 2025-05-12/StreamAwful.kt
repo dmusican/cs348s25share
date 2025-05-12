553855c5fb84a0bc9ddd37e5ecec9bcf625dcdd2
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-12 14:50:41 -0500

    Scripted auto-commit on change (2025-05-12 14:50:41) by gitwatch.sh

---
+++ 2025-05-12/StreamAwful.kt
@@ -0,0 +1,27 @@
+import java.util.stream.IntStream
+fun main() {
+
+    val myList = mutableListOf<Int>()
+    for (i in 0..<1000) {
+        myList.add(i)
+    }
+
+    // Streaming API: gives you iteration
+    // and other capabilities as function/method calls
+    // Very similar to higher order functions in Scheme
+    // (if you've seen that)
+    //
+    // parallel: functions that follow execute
+    // concurrently separately for each item
+    // in the stream
+    // using the Java ForkJoin thread pool
+    // myList.stream().parallel().forEach { print("$it ") }
+    // println()
+
+    // More calculations
+    val answer = IntStream.range(0, 10_000_000)
+        .parallel()
+        .mapToDouble { Math.sin(it.toDouble()) }
+        .reduce { a,b -> a + b }
+    println(answer)
+}
