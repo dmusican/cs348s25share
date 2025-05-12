c8e080c413fd147b9b01807a9661d7dff755fec9
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-12 14:42:08 -0500

    Scripted auto-commit on change (2025-05-12 14:42:08) by gitwatch.sh

---
+++ 2025-05-12/StreamFun.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val myList = mutableListOf<Int>()
+    for (i in 0..<1000) {
+        myList.add(i)
+    }
+
+    // Streaming API: gives you iteration
+    // and other capabilities as function/method calls
+    // Very similar to higher order functions in Scheme
+    // (if you've seen that)
+    myList.stream().forEach { print("$it ") }
+    println()
+}
