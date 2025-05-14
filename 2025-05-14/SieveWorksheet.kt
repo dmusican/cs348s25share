3a6dddf9b1159707e6d7629ea35a5cece66552d3
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-14 14:03:20 -0500

    Scripted auto-commit on change (2025-05-14 14:03:20) by gitwatch.sh

---
+++ 2025-05-14/SieveWorksheet.kt
@@ -0,0 +1,49 @@
+import java.util.stream.IntStream;
+
+fun main() {
+    val limit = 500000000 // Limit to find prime numbers up to
+
+    // Create a boolean array "isPrime" and initialize all entries as true
+    val isPrime= Array<Boolean>(limit+1) { true }
+
+    IntStream
+        // Use all integers up to the square root of the limit as the sieve choices
+
+        .rangeClosed(2, ___________________________________)
+
+        // Use only those that are know to be prime
+
+        .filter { ______________________________________________ }
+
+        // Set all multiples of that prime within range to be not prime
+        // Don't use a direct loop, do it via streams
+
+        .forEach { prime ->
+
+
+
+
+
+
+
+
+                 }
+        }
+
+    val count = IntStream.rangeClosed(2, limit)
+
+        // Keep just the prime numbers
+
+        .filter { _______________________________________________________}
+
+        .count()
+
+    println("Number of primes up to $limit: $count")
+}
+
+
+    // Followup questions:
+    // Where can .parallel() be added to speed things up?
+    //
+    // Where is a place that someone might be tempted to add .parallel(), but would
+    // just make things worse?
