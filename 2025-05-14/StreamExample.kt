5af20613f641c1e140c90613f5e96457314305c7
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-14 14:16:00 -0500

    Scripted auto-commit on change (2025-05-14 14:16:00) by gitwatch.sh

--- 2025-05-14/StreamExample.kt
+++ 2025-05-14/StreamExample.kt
@@ -9,6 +9,10 @@ fun main() {
             it
         }
         .filter { number -> number % 2 == 0 }
+        .map { it ->
+            println("you")
+            it
+        }
         .forEach { thing -> println(thing)}
 
 }
