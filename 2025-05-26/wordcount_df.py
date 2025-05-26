a07e0a3e6d8886a3ce6e5605fc6ceeeb1933e6ab
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-26 14:50:00 -0500

    Scripted auto-commit on change (2025-05-26 14:50:00) by gitwatch.sh

--- 2025-05-26/wordcount_df.py
+++ 2025-05-26/wordcount_df.py
@@ -10,15 +10,17 @@ spark = (SparkSession
 
 lines = spark.read.text(inputFilename)
 
-words = lines.flatMap(lambda line: line.split(" "))
-# print(words.take(50))
+lines.show()
 
-words_with_ones = words.map(lambda word: (word, 1))
-#print(words_with_ones.take(50))
+# words = lines.flatMap(lambda line: line.split(" "))
+# # print(words.take(50))
 
-# reduceByKey assumes key to aggregate is first of
-# tuple
-counts = (words_with_ones
-          .reduceByKey(lambda a, b: a+b)  #type: ignore
-          )
-print(counts.take(10))
+# words_with_ones = words.map(lambda word: (word, 1))
+# #print(words_with_ones.take(50))
+
+# # reduceByKey assumes key to aggregate is first of
+# # tuple
+# counts = (words_with_ones
+#           .reduceByKey(lambda a, b: a+b)  #type: ignore
+#           )
+# print(counts.take(10))
