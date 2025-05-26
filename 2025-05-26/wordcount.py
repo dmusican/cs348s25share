535be43f3cb235a46ea96362b472718dd65b84ec
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-26 14:34:24 -0500

    Scripted auto-commit on change (2025-05-26 14:34:24) by gitwatch.sh

--- 2025-05-26/wordcount.py
+++ 2025-05-26/wordcount.py
@@ -19,4 +19,4 @@ words_with_ones = words.map(lambda word: (word, 1))
 # reduceByKey assumes key to aggregate is first of
 # tuple
 counts = words_with_ones.reduceByKey(lambda a, b: a+b)
-print((counts.take(10))
+print(counts.take(10)
