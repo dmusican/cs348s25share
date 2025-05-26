f9a330aea4679aedefd80970ca0fa52819984dbd
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-26 14:48:21 -0500

    Scripted auto-commit on change (2025-05-26 14:48:21) by gitwatch.sh

---
+++ 2025-05-26/wordcount_df.py
@@ -0,0 +1,24 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+inputFilename = 'alice.txt'
+
+conf = (SparkConf()
+        .setAppName("WordCount")
+        .setMaster("local"))
+spark = SparkContext(conf=conf)
+
+lines = spark.textFile(inputFilename)
+
+words = lines.flatMap(lambda line: line.split(" "))
+# print(words.take(50))
+
+words_with_ones = words.map(lambda word: (word, 1))
+#print(words_with_ones.take(50))
+
+# reduceByKey assumes key to aggregate is first of
+# tuple
+counts = (words_with_ones
+          .reduceByKey(lambda a, b: a+b)  #type: ignore
+          )
+print(counts.take(10))
